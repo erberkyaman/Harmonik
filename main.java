b924c467c11c48057490c541b8b6643b7df9c316
erberkyaman <113280397+erberkyaman@users.noreply.github.com>
2022-12-07 23:52:49 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Harmonik {
+    public static void main(String[] args) {
+        int n;
+        double result=0;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("N Sayısı : ");
+        n= input.nextInt();
+
+        for (double i=1; i<=n;i++)
+        {
+            result+=(1/i);
+        }
+
+        System.out.print("Sonuç : "+ result);
+    }
+}
